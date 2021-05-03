d7ff5e1ba14e6983154c53295120b57d3655d9be
yuriDuque <yurithielmann83@gmail.com>
2021-05-03 12:41:39 -0300

    enviando projeto

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { routes } from "./routes";
+
+const app = express();
+
+console.log("starting...")
+
+// pegandos os arquivos .env e utilizando as variaveis de ambiente
+dotenv.config();
+
+// define como será feita a comunicação default do express
+app.use(express.json());
+
+// abilita a utilização do cors
+app.use(cors());
+
+app.use(routes);
+
+// pegando a porta do arquivo env
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {console.log(`Server is running: ${process.env.APP_URL}:${port}`)});
